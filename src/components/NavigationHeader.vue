d091b5335c078e11fc8b60ef01799256c647f800
yuhanna <yuhannakapali@gmail.com>
2023-06-18 20:10:33 +0545

    fixed the designed and enhanced the ui

--- src/components/NavigationHeader.vue
+++ src/components/NavigationHeader.vue
@@ -1,17 +1,25 @@
 <template>
-  <div class="container d-flex justify-content-between p-3">
+  <div class="container d-flex justify-content-between p-3 align-items-center">
     <div class="branding">
       <RouterLink to="/">
-        <h2 style="font-weight: 400; font-style: italic">Dmcommerce</h2>
+        <div class="d-flex align-items-center">
+          <img src="@/assets/logo.png" alt="" />
+          <div>
+            <p class="branding">DMCOMMERCE</p>
+            <p class="branding-subtitle">Smart Data Driven Marketing for eCommerce</p>
+          </div>
+        </div>
       </RouterLink>
     </div>
     <nav>
       <b-nav>
         <b-nav-item>
-          <RouterLink to="/example-template"><h6>Example Template</h6> </RouterLink>
+          <RouterLink to="/example-template"><p class="nav-link">Example Template</p> </RouterLink>
         </b-nav-item>
         <b-nav-item>
-          <RouterLink to="/newsletter-subscription"><h6>Newsletter Subscription</h6></RouterLink>
+          <RouterLink to="/newsletter-subscription">
+            <p class="nav-link">Newsletter Subscription</p>
+          </RouterLink>
         </b-nav-item>
       </b-nav>
     </nav>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,24 @@
 <template>
-  <div class="container vh-100 d-flex justify-content-center">
-    <p>This was task given by the Dmcommerce</p>
+  <div class="container d-flex justify-content-center">
+    <div class="">
+      <h5 class="text-center title text-underlined tuo-8">Dmcommerce Task</h5>
+      <p class="">
+        The task involves creating a Vue 3 application with two routes: "example—template" and
+        "newsletter—subscription" . A simple menu to navigate between the two routes should be
+        always visible When a route is loaded, the application should make an API call to obtain a
+        JSON object that describes a form to be rendered in the view. The JSON objects downloaded
+        via API calls should be saved in a pin ia storage, and subsequently loaded from storage
+        instead Of using an HTTP call if the same route is loaded again in the same session
+        TypeScript should be used if possible, but it's Okay to use JavaScript if you re not
+        experienced with TypeScript. Attached are: — two JSON files to simulate API calls via fetch;
+        — a sample design for the form (it doesn't need to be pixel—perfect, serves as an indication
+        of style).
+      </p>
+      <h6 class="text-center sub-title">Mock Provided</h6>
+      <div class="d-flex justify-content-center">
+        <img src="@/assets/mockup.png" class="mockup-image" alt="" />
+      </div>
+    </div>
   </div>
 </template>
 
